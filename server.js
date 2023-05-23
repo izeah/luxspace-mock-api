e381dcc31a6d91bcb4dc088b247cd5459607717b
Faiz Mohammad Hafidza <faizmohhafidza@gmail.com>
2023-05-23 14:46:10 +0700

    add endpoint checkout POST

--- server.js
+++ server.js
@@ -4,6 +4,64 @@ const router = jsonServer.router("./db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+server.use(jsonServer.bodyParser);
+server.post("/api/v1/checkout", (req, res, next) => {
+  const { completeName, emailAddress, address, phoneNumber, courier, payment } =
+    req.body;
+
+  const resp = {
+    success: false,
+    msg: "",
+    data: null,
+  };
+
+  try {
+    if (!completeName || completeName == "") {
+      resp.msg = ["completeName is empty"];
+      return res.status(400).json(resp);
+    }
+    if (
+      !emailAddress ||
+      typeof emailAddress !== "string" ||
+      emailAddress == "" ||
+      !emailAddress
+        .toLowerCase()
+        .match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)
+    ) {
+      resp.msg = ["emailAddress is empty or invalid format"];
+      return res.status(400).json(resp);
+    }
+    if (!address || address == "") {
+      resp.msg = ["address is empty"];
+      return res.status(400).json(resp);
+    }
+    if (
+      !phoneNumber ||
+      typeof phoneNumber !== "string" ||
+      phoneNumber == "" ||
+      phoneNumber.toLowerCase().match(/^08\d\S{9,13}$/)
+    ) {
+      resp.msg = ["phoneNumber is empty or invalid format"];
+      return res.status(400).json(resp);
+    }
+    if (!courier || typeof courier !== "number" || courier <= 0) {
+      resp.msg = ["courier must be filled by number"];
+      return res.status(400).json(resp);
+    }
+    if (!payment || typeof payment !== "number" || payment <= 0) {
+      resp.msg = ["payment must be filled by number"];
+      return res.status(400).json(resp);
+    }
+
+    resp.success = true;
+    resp.msg = "Request Successfully Proceed";
+    return res.status(201).json(resp);
+  } catch (error) {
+    resp.msg = "Something goes wrong";
+    return res.status(500).json(resp);
+  }
+});
+
 // Add this before server.use(router)
 server.use(
   jsonServer.rewriter({
